7952377e067fb6f8943ce336f86f9b484b9bd689
Susumu Yamazaki <zacky1972@gmail.com>
2020-12-15 21:02:35 +0900

    implement pmap_2

--- bench/basic_bench.exs
+++ bench/basic_bench.exs
@@ -1,15 +1,20 @@
 defmodule BasicBench do
   use Benchfella
 
-  @range_0x1000 (1..0x1000)
+  @range_0x10000 (1..0x10000)
 
   bench "Enum" do
-    @range_0x1000
+    @range_0x10000
     |> Enum.map(&(&1 * &1))
   end
 
   bench "Pmap" do
-    @range_0x1000
+    @range_0x10000
     |> Parallel.pmap(&(&1 * &1))
+  end
+
+  bench "Pmap2" do
+    @range_0x10000
+    |> Parallel.pmap_2(&(&1 * &1))
   end
 end
--- lib/parallel.ex
+++ lib/parallel.ex
@@ -1,7 +1,30 @@
 defmodule Parallel do
   def pmap(collection, func) do
-  	collection
-  	|> Enum.map(&(Task.async(fn -> func.(&1) end)))
-  	|> Enum.map(&Task.await/1)
+    collection
+    |> Enum.map(&Task.async(fn -> func.(&1) end))
+    |> Enum.map(&Task.await/1)
+  end
+
+  def pmap_2(collection, func) do
+    pmap_sub(Enum.to_list(collection), [], func, 0, 2000)
+    |> Enum.map(&Task.await/1)
+    |> List.flatten()
+    |> Enum.reverse()
+  end
+
+  defp pmap_sub([], rest, func, _, _) do
+    [Task.async(fn -> Enum.map(rest, func) end)]
+  end
+
+  defp pmap_sub(rest, heads, func, threshold, threshold) do
+    [
+      Task.async(fn -> heads |> Enum.map(func) end)
+      | pmap_sub(rest, [], func, 0, threshold) |> Enum.reverse()
+    ]
+    |> Enum.reverse()
+  end
+
+  defp pmap_sub([head | tail], heads, func, count, threshold) do
+    pmap_sub(tail, [head | heads], func, count + 1, threshold)
   end
 end
