5f13db8f4ea2cd9432814b1477269c4e4ae43ba7
Susumu Yamazaki <zacky1972@gmail.com>
2020-12-16 11:11:19 +0900

    change benchmark into logistic_map

--- bench/basic_bench.exs
+++ bench/basic_bench.exs
@@ -9,28 +9,45 @@ defmodule BasicBench do
     {:ok, nil}
   end
 
+  def logistic_map(v) do
+    rem(22 * v * (v + 1), 6_700_417)
+  end
+
+  def logistic_map_10(v) do
+    logistic_map(v)
+    |> logistic_map()
+    |> logistic_map()
+    |> logistic_map()
+    |> logistic_map()
+    |> logistic_map()
+    |> logistic_map()
+    |> logistic_map()
+    |> logistic_map()
+    |> logistic_map()
+  end
+
   bench "Enum" do
     @range_0x10000
-    |> Enum.map(&(&1 * &1))
+    |> Enum.map(& logistic_map_10(&1))
   end
 
   bench "Pmap" do
     @range_0x10000
-    |> Parallel.pmap(&(&1 * &1))
+    |> Parallel.pmap(& logistic_map_10(&1))
   end
 
   bench "Pmap2" do
     @range_0x10000
-    |> Parallel.pmap_2(&(&1 * &1))
+    |> Parallel.pmap_2(& logistic_map_10(&1))
   end
 
   bench "Pmap3" do
     @range_0x10000
-    |> Parallel.pmap_3(&(&1 * &1))
+    |> Parallel.pmap_3(& logistic_map_10(&1))
   end
 
   bench "Pmap4" do
     @range_0x10000
-    |> Parallel.pmap_4(&(&1 * &1))
+    |> Parallel.pmap_4(& logistic_map_10(&1))
   end
 end
