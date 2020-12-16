23aeed423f164714b6917dc134321b9e618b2cf7
Susumu Yamazaki <zacky1972@gmail.com>
2020-12-16 12:28:16 +0900

    create Pelemay benchmark

--- bench/basic_bench.exs
+++ bench/basic_bench.exs
@@ -6,55 +6,44 @@ defmodule BasicBench do
   setup_all do
     Parallel.Application.start(nil, nil)
     Parallel.init()
+    LogisticMap.logistic_map_10_pelemay(1..0x10000)
     {:ok, nil}
-  end
-
-  def logistic_map(v) do
-    rem(22 * v * (v + 1), 6_700_417)
-  end
-
-  def logistic_map_10(v) do
-    logistic_map(v)
-    |> logistic_map()
-    |> logistic_map()
-    |> logistic_map()
-    |> logistic_map()
-    |> logistic_map()
-    |> logistic_map()
-    |> logistic_map()
-    |> logistic_map()
-    |> logistic_map()
   end
 
   bench "Enum" do
     @range_0x10000
-    |> Enum.map(& logistic_map_10(&1))
+    |> Enum.map(& LogisticMap.logistic_map_10(&1))
   end
 
   bench "Pmap" do
     @range_0x10000
-    |> Parallel.pmap(& logistic_map_10(&1))
+    |> Parallel.pmap(& LogisticMap.logistic_map_10(&1))
   end
 
   bench "Pmap2" do
     @range_0x10000
-    |> Parallel.pmap_2(& logistic_map_10(&1))
+    |> Parallel.pmap_2(& LogisticMap.logistic_map_10(&1))
   end
 
   bench "Pmap3" do
     @range_0x10000
-    |> Parallel.pmap_3(& logistic_map_10(&1))
+    |> Parallel.pmap_3(& LogisticMap.logistic_map_10(&1))
   end
 
   bench "Pmap4" do
     @range_0x10000
-    |> Parallel.pmap_4(& logistic_map_10(&1))
+    |> Parallel.pmap_4(& LogisticMap.logistic_map_10(&1))
   end
 
   bench "Flow" do
     @range_0x10000
     |> Flow.from_enumerable()
-    |> Flow.map(& logistic_map_10(&1))
+    |> Flow.map(& LogisticMap.logistic_map_10(&1))
     |> Enum.sort()
-  end    
+  end
+
+  bench "Pelemay" do
+    @range_0x10000
+    |> LogisticMap.logistic_map_10_pelemay()
+  end
 end
